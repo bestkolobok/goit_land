1f14e34048a385316d7708cc9548d930ef4ac8d5
Alex <alex.guzey@gmail.com>
2018-05-05 12:21:03 +0300

    add main page, add header

---
+++ src/components/MainPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <span>Hello {{VariableOne}}!</span>
+        <span>in {{VariableTwo}}</span>
+    </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'MainPage',
+  data(){
+    return{
+        VariableOne: 'World',
+        VariableTwo: 'GoIt'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import MainPage from '@/components/MainPage'
+
+
+
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        { path: '/', name: 'MainPage', component: MainPage },
+        // { path: '/movies', name: 'movies', component: Search },
+        // { path: '/series', name: 'series', component: Search },
+        // { path: '/actors', name: 'actors', component: Search },
+    ]
 })
